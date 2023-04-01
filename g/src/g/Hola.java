2b84f005ee8d9529fba3a0299f4ff0496fd0ca69
JL <JL@DESKTOP-2943O1E>
2023-04-01 20:16:52 +0200

    hola1

---
+++ g/src/g/Hola.java
@@ -0,0 +1,21 @@
+package g;
+
+import java.util.Scanner;
+
+public class Hola {
+
+public static void main(String[] args) {
+	
+	
+String nombre ="juan";
+int edad = 20;
+double peso = 75.55;
+long pi = 31416;
+byte dias = 127; //no puede superar 128
+
+Scanner scan1 = new Scanner (System.in);
+System.out.println("introduce nombre ");
+nombre = scan1.nextLine();
+System.out.println(nombre);}}
+
+
